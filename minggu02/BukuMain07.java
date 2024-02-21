b00e3f8717e24c674e6cfb4064ea344f4900115e
azzazhr <azzahraqina70@gmail.com>
2024-02-21 08:52:50 +0700

    add Percobaan 2 (BukuMain)

---
+++ minggu02/BukuMain07.java
@@ -0,0 +1,19 @@
+package minggu02;
+
+public class BukuMain07 {
+    public static void main(String[] args) {
+
+        Buku07 bk1 = new Buku07();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+
+    }
+}
