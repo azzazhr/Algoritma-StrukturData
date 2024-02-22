3e3093271764cdb13e19ebf7ac8998d5da06de97
azzazhr <azzahraqina70@gmail.com>
2024-02-22 13:46:28 +0700

    add latihan 1

--- minggu02/BukuMain07.java
+++ minggu02/BukuMain07.java
@@ -13,15 +13,23 @@ public class BukuMain07 {
         bk1.tampilInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
+        bk1.hitungDiskon();
         bk1.tampilInformasi();
 
         Buku07 bk2 = new Buku07("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
+        bk1.hitungDiskon();
         bk2.tampilInformasi();
 
         Buku07 Azza = new Buku07("Laskar Pelangi", "Andrea Hirata", 259, 30, 80000);
         Azza.terjual(15);
+        Azza.hitungDiskon();
         Azza.tampilInformasi();
+
+        Buku07 bk3 = new Buku07("Daun Yang Jatuh", "Tere Liye", 547, 53, 183000);
+        bk3.terjual(12);
+        bk3.hitungDiskon();
+        bk3.tampilInformasi();
 
     }
 }
