044e40f8baddad22dd7706997a14b682a854106d
azzazhr <azzahraqina70@gmail.com>
2024-02-22 13:46:20 +0700

    add latihan1

--- minggu02/Buku07.java
+++ minggu02/Buku07.java
@@ -2,7 +2,8 @@ package minggu02;
 
 public class Buku07 {
     String judul, pengarang;
-    int halaman, stok, harga;
+    int halaman, stok, harga, hargaTot;
+    double hargaBayar, diskon = 0.0;
 
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
@@ -10,12 +11,17 @@ public class Buku07 {
         System.out.println("Jumlah halaman: " + halaman);
         System.out.println("Sisa stok: " + stok);
         System.out.println("Harga: Rp " + harga);
+        System.out.println("Total Harga Penjualan Rp " + hargaTot);
+        System.out.println("Harga bayar setelah diskon: " + hargaBayar);
+        System.out.println("========================");
 
     }
 
     void terjual(int jml) {
         if (stok > 0) {
             stok -= jml;
+            System.out.println("Terjual: " + jml + "buku");
+            hitungHargaTotal(jml);
         } else {
             System.out.println("Tidak ada stok");
         }
@@ -27,6 +33,27 @@ public class Buku07 {
 
     void gantiHarga(int hrg) {
         harga = hrg;
+    }
+
+    void hitungHargaTotal(int jml) {
+        hargaTot = harga * jml;
+    }
+
+    void hitungDiskon() {
+        if (hargaTot > 150000) {
+            diskon = 0.12 * hargaTot;
+            System.out.println("Anda mendapatkan diskon sebesar 12%");
+        } else if (hargaTot >= 75000 && hargaTot <= 150000) {
+            diskon = 0.05 * hargaTot;
+            System.out.println("Anda mendapatkan diskon sebesar 5%");
+        } else {
+            System.out.println("Anda tidak mendapatkan diskon");
+        }
+        hitungHargaBayar(diskon);
+    }
+
+    void hitungHargaBayar(double disc) {
+        hargaBayar = hargaTot - disc;
     }
 
     public Buku07() {
