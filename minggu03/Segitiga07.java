034b891ecff480dfb9343191a54236e6f9863351
azzazhr <azzahraqina70@gmail.com>
2024-02-28 23:00:42 +0700

    add segitiga

---
+++ minggu03/Segitiga07.java
@@ -0,0 +1,17 @@
+package minggu03;
+
+public class Segitiga07 {
+    public static void main(String[] args) {
+        Segitiga[] sgArray = new Segitiga[4];
+
+        sgArray[0] = new Segitiga(10, 4);
+        sgArray[1] = new Segitiga(20, 10);
+        sgArray[2] = new Segitiga(15, 6);
+        sgArray[3] = new Segitiga(25, 10);
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.println("sgArray ke-" + i + "\talas: " +
+                    sgArray[i].alas + ", tinggi: " + sgArray[i].tinggi);
+        }
+    }
+
+}
