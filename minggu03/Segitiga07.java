267548d201b4263c101f9f6374538c34d4191d72
azzazhr <azzahraqina70@gmail.com>
2024-02-28 23:06:49 +0700

    add Array'Segitiga' percobaan 3 no 5

--- minggu03/Segitiga07.java
+++ minggu03/Segitiga07.java
@@ -8,9 +8,17 @@ public class Segitiga07 {
         sgArray[1] = new Segitiga(20, 10);
         sgArray[2] = new Segitiga(15, 6);
         sgArray[3] = new Segitiga(25, 10);
+
         for (int i = 0; i < sgArray.length; i++) {
-            System.out.println("sgArray ke-" + i + "\talas: " +
+            System.out.println("sgArray ke-" + i + "\talas: a" +
                     sgArray[i].alas + ", tinggi: " + sgArray[i].tinggi);
+        }
+
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.println("Luas segitiga ke-" + i + ": " +
+                    sgArray[i].hitungLuas());
+            System.out.println("Keliling segitiga ke-" + i + ": " +
+                    sgArray[i].hitungKeliling());
         }
     }
 }
