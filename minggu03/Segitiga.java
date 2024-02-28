ae72544b48ec53c2b0655d89e7d169ddce5e3a31
azzazhr <azzahraqina70@gmail.com>
2024-02-28 23:00:28 +0700

    add class'Segitiga' percobaan 3 no 2

---
+++ minggu03/Segitiga.java
@@ -0,0 +1,13 @@
+package minggu03;
+
+public class Segitiga {
+
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+
+}
