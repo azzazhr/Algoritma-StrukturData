b59989744659c5da798e51e0601b3b3d8cc38e28
azzazhr <azzahraqina70@gmail.com>
2024-03-19 22:11:45 +0700

    add mainsum(percobaan 3)

---
+++ minggu05/MainSum07.java
@@ -0,0 +1,29 @@
+package minggu05;
+
+import java.util.Scanner;
+
+public class MainSum07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================================================");
+        System.out.println("Program Menghitung Keuntungan Total (Satuan Juta. Misal 5.9)");
+        System.out.println("Masukkan Jumlah Bulan: ");
+        int elm = sc.nextInt();
+
+        Sum07 sm = new Sum07(elm);
+        System.out.println("================================================================");
+        for (int i = 0; i < sm.elemen; i++) {
+            System.out.println("Masukkan Untung Bulan ke - " + (i + 1) + " = ");
+            sm.keuntungan[i] = sc.nextDouble();
+        }
+
+        System.out.println("================================================================");
+        System.out.println("AlGORITMA BRUCE FORCE");
+        System.out.println(
+                "Total Keuntungan Perusahaan Selama " + sm.elemen + " Bulan Adalah = " + sm.totalBF(sm.keuntungan));
+        System.out.println("================================================================");
+        System.out.println("ALGORITMA DIVIDE CONQUER");
+        System.out.println("Total Keuntungan Perusahaan Selama " + sm.elemen + " Bulan Adalah = "
+                + sm.totalDC(sm.keuntungan, 0, sm.elemen - 1));
+    }
+}
