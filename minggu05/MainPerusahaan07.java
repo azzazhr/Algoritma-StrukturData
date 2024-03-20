6e93f57acd9ad5454c674ed308fdd3fd2d52dadc
azzazhr <azzahraqina70@gmail.com>
2024-03-21 05:43:43 +0700

    add perusahaan(percobaan3 no 3)

---
+++ minggu05/MainPerusahaan07.java
@@ -0,0 +1,42 @@
+package minggu05;
+
+import java.util.Scanner;
+
+public class MainPerusahaan07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Membaca jumlah perusahaan
+        System.out.println("================================================================");
+        System.out.println("Program Menghitung Keuntungan Total Beberapa Perusahaan (Satuan Juta. Misal 5.9)");
+        System.out.println("Masukkan Jumlah Perusahaan: ");
+        int jumlahPerusahaan = sc.nextInt();
+
+        // Menyimpan data keuntungan per perusahaan
+        Perusahaan07[] perusahaan = new Perusahaan07[jumlahPerusahaan];
+        for (int i = 0; i < jumlahPerusahaan; i++) {
+            System.out.println("================================================================");
+            System.out.println("Data Perusahaan " + (i + 1));
+
+            // Membaca jumlah bulan per perusahaan
+            System.out.println("Masukkan Jumlah Bulan: ");
+            int jumlahBulan = sc.nextInt();
+
+            perusahaan[i] = new Perusahaan07(jumlahBulan);
+
+            // Membaca keuntungan per bulan
+            for (int j = 0; j < jumlahBulan; j++) {
+                System.out.println("Masukkan Untung Bulan ke - " + (j + 1) + " = ");
+                perusahaan[i].keuntungan[j] = sc.nextDouble();
+            }
+        }
+
+        // Menghitung dan menampilkan total keuntungan per perusahaan
+        for (int i = 0; i < jumlahPerusahaan; i++) {
+            System.out.println("================================================================");
+            System.out.println("Perusahaan " + (i + 1));
+            System.out.println("Total Keuntungan = " + perusahaan[i].totalBF());
+            System.out.println("================================================================");
+        }
+    }
+}
