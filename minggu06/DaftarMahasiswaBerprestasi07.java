9117417b948040b8580dd8b4429e6784267f1364
azzazhr <azzahraqina70@gmail.com>
2024-03-25 09:10:46 +0700

    add MainDaftarMahasiswaBerprestasi(percobaan1)

---
+++ minggu06/DaftarMahasiswaBerprestasi07.java
@@ -0,0 +1,35 @@
+package minggu06;
+
+public class DaftarMahasiswaBerprestasi07 {
+    Mahasiswa07 ListMhs[] = new Mahasiswa07[5];
+    int idx;
+
+    void tambah(Mahasiswa07 m) {
+        if (idx < ListMhs.length) {
+            ListMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh!!");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa07 m : ListMhs) {
+            m.tampil();
+            System.out.println("========================");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < ListMhs.length - 1; i++) {
+            for (int j = 1; j < ListMhs.length - i; j++) {
+                if (ListMhs[j].ipk > ListMhs[j - 1].ipk) {
+
+                    Mahasiswa07 tmp = ListMhs[j];
+                    ListMhs[j] = ListMhs[j - 1];
+                    ListMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
