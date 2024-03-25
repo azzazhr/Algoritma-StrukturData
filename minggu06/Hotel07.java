d5683b02ec856b59daa1ca66c7e435f8cdeb1ee4
azzazhr <azzahraqina70@gmail.com>
2024-03-25 11:44:23 +0700

    add ClassHotel(latihan praktikum)

---
+++ minggu06/Hotel07.java
@@ -0,0 +1,21 @@
+package minggu06;
+
+public class Hotel07 {
+    String nama, kota;
+    int harga;
+    byte bintang;
+
+    Hotel07(String n, String k, int h, byte b) {
+        nama = n;
+        kota = k;
+        harga = h;
+        bintang = b;
+    }
+
+    void tampil() {
+        System.out.println("Nama Hotel = " + nama);
+        System.out.println("Kota = " + kota);
+        System.out.println("Harga = " + harga);
+        System.out.println("Bintang = " + bintang);
+    }
+}
