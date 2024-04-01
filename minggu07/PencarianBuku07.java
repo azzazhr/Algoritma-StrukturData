d48ed328a07705efa9b19569c6531b692d944a12
azzazhr <azzahraqina70@gmail.com>
2024-04-01 10:20:15 +0700

    add pencarianBuku(percobaan2)

---
+++ minggu07/PencarianBuku07.java
@@ -0,0 +1,54 @@
+package minggu07;
+
+public class PencarianBuku07 {
+
+    Buku07 listBk[] = new Buku07[5];
+    int idx;
+
+    void tambah(Buku07 m) {
+        if (idx < listBk.length) {
+            listBk[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (int i = 0; i < listBk.length; i++) {
+            Buku07 buku = listBk[i];
+            buku.tampilDataBuku();
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int j = 0; j < listBk.length; j++) {
+            if (listBk[j].kodeBuku == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void TampilPosisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("Data " + x + " tidak ditemukan");
+        }
+    }
+
+    public void TampilData(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Kode Buku\t: " + x);
+            System.out.println("Judul\t\t: " + listBk[pos].judulBuku);
+            System.out.println("Tahun Terbit\t: " + listBk[pos].tahunTerbit);
+            System.out.println("Pengarang\t: " + listBk[pos].pengarang);
+            System.out.println("Stock\t\t: " + listBk[pos].stock);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+}
