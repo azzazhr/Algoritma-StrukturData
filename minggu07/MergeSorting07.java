e608ccb0d8b193f7d27b3c3ad3c4b082b2e7250a
azzazhr <azzahraqina70@gmail.com>
2024-04-01 14:08:37 +0700

    add classMergeSort(percobaan3 merge sort)

--- minggu07/MergeSorting07.java
+++ minggu07/MergeSorting07.java
@@ -43,7 +43,7 @@ public class MergeSorting07 {
 
     public void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
