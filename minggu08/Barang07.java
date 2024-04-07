fb0dfe28cc777c2498a986fba12944f8475ba3b4
azzazhr <azzahraqina70@gmail.com>
2024-04-07 07:40:07 +0700

    add ClassBarang(percobaan1 stack)

---
+++ minggu08/Barang07.java
@@ -0,0 +1,12 @@
+package minggu08;
+
+public class Barang07 {
+    int kode;
+    String nama, kategori;
+
+    public Barang07(int kd, String nm, String ktg) {
+        kode = kd;
+        nama = nm;
+        kategori = ktg;
+    }
+}
