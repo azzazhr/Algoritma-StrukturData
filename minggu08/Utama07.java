2b5c0790d8bbfd62eaa62145b72e8cdd2f5d45d6
azzazhr <azzahraqina70@gmail.com>
2024-04-07 08:43:42 +0700

    add MainUtama(modify percobaan1 stack no1)

--- minggu08/Utama07.java
+++ minggu08/Utama07.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class Utama07 {
     public static void main(String[] args) {
-        Gudang07 gudang = new Gudang07(7);
+        // Gudang07 gudang = new Gudang07(7);
         Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas gudang : ");
+        int kapasitasGudang = sc.nextInt();
+        sc.nextLine();
+        Gudang07 gudang = new Gudang07(kapasitasGudang);
 
         while (true) {
             System.out.println("\nMENU:");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Ambil Barang");
             System.out.println("3. Tampilkan Tumpukan Barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Tampilkan Barang Teratas");
+            System.out.println("5. Tampilkan Barang Terbawah");
+            System.out.println("6. Cari Barang");
+            System.out.println("7. Keluar");
             System.out.print("Pilih Operasi: ");
             int pilihan = sc.nextInt();
             sc.nextLine();
@@ -39,7 +46,23 @@ public class Utama07 {
                     break;
 
                 case 4:
+                    gudang.lihatBarangTeratas();
                     break;
+
+                case 5:
+                    gudang.lihatBarangTerbawah();
+                    break;
+
+                case 6:
+                    System.out.print("Masukkann kode barang yang dicari:");
+                    kode = sc.nextInt();
+                    sc.nextLine();
+                    gudang.cariBarang(kode);
+                    break;
+
+                case 7:
+                    System.exit(0);
+
                 default:
                     System.out.println("Pilihan tidak valid. Silahkan coba lagi");
             }
